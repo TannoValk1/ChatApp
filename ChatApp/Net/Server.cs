8b78d800583b0ca4f6569c825eafc3801990d06f
opilane <opilane@HP-CZC1339D28>
2025-03-14 11:08:06 +0200

    22:11

---
+++ ChatApp/Net/Server.cs
@@ -0,0 +1,27 @@
+﻿using ChatClient.Net.IO;
+using System;
+using System.Net.Sockets;
+
+
+namespace ChatClient.Net
+{
+    class Server
+    {
+        TcpClient _client;
+        public Server()
+        {
+            _client = new TcpClient();
+        }
+        public void ConnectToServer(string username)
+        {
+            if(!_client.Connected)
+            {
+                _client.Connect("127.0.0.1", 7891);
+                var connectPacket = new PacketBuilder();
+                connectPacket.WriteOpCode(0);
+                connectPacket.WriteString(username);
+                _client.Client.Send(connectPacket.GetPacketBytes());
+            }
+        }
+    }
+}
